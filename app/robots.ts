014c4187985204230269fadf8c469237e10a7c79
khadijazainab421-Datascientist <khadijazainab421@gmail.com>
2026-08-07 10:46:44 -0700

    robots txt added

---
+++ app/robots.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = "https://vynex.pk";
+
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/api/",
+          "/_next/",
+        ],
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
+  };
+}
